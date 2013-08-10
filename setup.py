cfbd48e956d28030a51e2ce8c7bca54932060692
Joe Lei <thezero12@hotmail.com>
2013-08-10 11:14:38 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='project-euler-python',
+    version='0.0.2',
+    description='project euler problem solve by python',
+    author='Joe Lei',
+    author_email='thezero12@hotmail.com',
+    url='http://ifooth.com',
+    packages=['euler'],	
+    )
