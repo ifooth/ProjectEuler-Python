3e36aac0985a4714de238cd8944de0d512416e18
Joe Lei <thezero12@hotmail.com>
2013-08-10 12:02:19 +0800

    add classifiers

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from distutils.core import setup
-setup(name='project-euler-python',
-    version='0.0.2',
+
+setup(
+    name='project-euler-python',
+    version='0.0.3',
     description='project euler problem solve by python',
     author='Joe Lei',
     author_email='thezero12@hotmail.com',
     url='http://ifooth.com',
-    packages=['euler'],	
-    )
+    packages=['euler'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Scientific/Engineering :: Mathematics',
+   ],
+)
